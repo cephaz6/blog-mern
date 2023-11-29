971f072b2aa2d2c3c92cf4093bdc9a80e78429e8
Yekeen Abiodun <abiodunyekeen76@gmail.com>
2023-11-29 13:34:56 +0000

    update .env

--- Backend/Helpers/database/connectDatabase.js
+++ Backend/Helpers/database/connectDatabase.js
@@ -1,10 +1,9 @@
 const mongoose = require("mongoose")
 
-MONGO_URI="mongodb://abiodunyekeen76:myproject@ac-q90drpn-shard-00-00.6lez2mv.mongodb.net:27017,ac-q90drpn-shard-00-01.6lez2mv.mongodb.net:27017,ac-q90drpn-shard-00-02.6lez2mv.mongodb.net:27017/?ssl=true&replicaSet=atlas-11okep-shard-0&authSource=admin&retryWrites=true&w=majority"
 
 const connectDatabase =async  () => {
 
-    await mongoose.connect(MONGO_URI ,{useNewUrlParser : true})
+    await mongoose.connect(process.env.MONGO_URI ,{useNewUrlParser : true})
 
     console.log("MongoDB Connection Successfully")
     
